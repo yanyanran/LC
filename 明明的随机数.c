4262fd07ce22b22289e48295e68620f29c7d259d
yan ran <93911762+yanyanran@users.noreply.github.com>
2022-01-13 11:43:30 +0800

    Update 明明的随机数.c

--- 明明的随机数.c
+++ 明明的随机数.c
@@ -1,42 +1,4 @@
-/*错误示范
-#include <stdio.h>
-int main ()
-{
-    int num[105]={0};
-    int N,i,j;
-    scanf("%d",&N); //N个随机数
-    if(N<=100&&N>0)
-    for(i=0;i<N;i++)          //安排随机数
-    {
-        scanf("%d",&num[i]);  
-        if(i==0)              //排重
-        {
-            break;
-        }else for(j=i-1;j>0;j--)
-        {
-            if(num[i]==num[j])
-            {
-                num[i]=0;
-                i--;
-                N--;
-                break;
-            }
-        }
-    }
-    		for(i=0;i<N;i++)
-            {
-			for(j=i+1;j<N;j++)
-            {
-				if(num[i]>num[j])
-				{
-					int c=num[i];
-					num[i]=num[j];
-					num[j]=c;
-				}
-            }
-            }
-   	return 0;
-}*/
+//方法一：查重+排序
 #include <stdio.h>
 int main ()
 {
@@ -50,21 +12,21 @@ int main ()
             scanf("%d",&t);  
             for(j=0;j<i;j++)
             {
-                if(t==num[j])  //相同跳出
+                if(t==num[j])  //遍历以往数组，相同跳出
                 {
                     i--;
                     N--;
                     break;
                 }
             }
-                if(j==i)        //否则存入数组
+                if(j==i)        //i完整，说明没有相同情况，存入数组
                 {
                     num[i]=t;
                 }
             //完成去重
         }
     
-    for(i=0;i<N;i++)          //排序
+    for(i=0;i<N;i++)          //排序（来个冒泡）
         for(j=i+1;j<N;j++)
             if(num[i]>num[j])
             {
@@ -81,6 +43,33 @@ int main ()
             printf("%d ",num[i]);
         }
     }else;
+    }
+    return 0;
+}
+
+//方法二：对号入座(可以省略排序步骤，高效便捷！！)
+#include<stdio.h>
+int main()
+{
+    int N,a[1001]={0},t,i;
+    int count=0; 
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&t);
+        if(a[t]==0)
+        {
+            a[t]=t;
+            count++;
+        }
+    }
+    printf("%d\n",count);
+    for(i=0;i<1001;i++)
+    {
+        if(a[i]!=0)
+        {
+            printf("%d ",a[i]);
+        }
     }
     return 0;
 }
