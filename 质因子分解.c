8ea4d91181b6d2ecbe24e99d6e78424d2565cfdc
yan ran <93911762+yanyanran@users.noreply.github.com>
2022-02-27 23:29:18 +0800

    Update 质因子分解.c

--- 质因子分解.c
+++ 质因子分解.c
@@ -1,35 +1,23 @@
-#include <stdio.h>
-int n,a[10001];
-void divi(int x)//分解质因数
+#include<stdio.h>
+int main()
 {
-    for (int i=2;i<=x;i++)//小于x就全部枚举
+    int n,a[10001];
+    scanf("%d",&n);
+    for(int i=2;i<=n;i++)//将n分解质因数
     {
-        int t=i;//保存在临时变量中
-        for (int j=2;j<=i;j++)//内循环枚举
+        int t=i;
+        for(int j=2;j<=i;j++)
         {
-            while (t%j==0)//如果能整除
+            while(t%j==0)//能整除
             {
                 a[j]++;//标注不再为0
                 t/=j;//除以j
             } 
         }
     }
-}
-void out()//输出
-{
-    for (int i=1;i<=n;i++) // 只要找小于n的就行了
+    for(int i=1;i<=n;i++) //输出
     {
-        if (a[i]!=0)//如果前面出现过了
-        {
-            printf("%d %d\n",i,a[i]);//先输出底数，再输出指数，注意千万不要忘了换行
-        }
+        if(a[i]!=0) printf("%d %d\n",i,a[i]);
     }
-}
-int main()
-{
-    memset(a,0,sizeof(a));//初始化a数组
-    scanf("%d",&n);//输入
-    divi(n);//将n分解质因数
-    out();//输出
-    return 0;//结束整个程序
+    return 0;
 }
